3554a7ca1a87aa1e3f450e9d71de2cce5c7ab82c
Fred White <fred-white@hotmail.co.uk>
2022-04-22 23:25:34 +0100

    refactor(initialise): remove variable that wasnt being used

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ require("core.colourscheme")
 
 -- Speed up loading Lua modules in Neovim to improve startup time.
 -- impatient needs to be setup before any other lua plugin is loaded
-local status_ok, impatient = pcall(require, "impatient")
+local status_ok, _ = pcall(require, "impatient")
 if not status_ok then
   return
 end
