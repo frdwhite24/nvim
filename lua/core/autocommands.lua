a8c9b5e39459a54cffc3f263a3b66ddea60d1dc9
Fred White <fred-white@hotmail.co.uk>
2022-05-12 11:41:31 +0100

    feat(cursorline): centralise on all movements, makes vim experience very nice!
    fix #17

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,25 +1,27 @@
-local utils = require('core.utils')
+local utils = require("core.utils")
 
 utils.define_augroups({
-    _general_settings = {
-        -- Disable continuation of comments on next line
-        {'BufWinEnter', '*', 'lua vim.opt.formatoptions:remove({\'c\', \'o\'})'},
-        {'BufRead', '*', 'lua vim.opt.formatoptions:remove({\'c\', \'o\', \'r\'})'},
-        {'BufNewFile', '*', 'lua vim.opt.formatoptions:remove({\'c\', \'o\', \'r\'})'},
+	_general_settings = {
+		-- Disable continuation of comments on next line
+		{ "BufWinEnter", "*", "lua vim.opt.formatoptions:remove({'c', 'o'})" },
+		{ "BufRead", "*", "lua vim.opt.formatoptions:remove({'c', 'o', 'r'})" },
+		{ "BufNewFile", "*", "lua vim.opt.formatoptions:remove({'c', 'o', 'r'})" },
 
-        -- Highlight on yank
-        {'TextYankPost', '*', 'silent! lua vim.highlight.on_yank()'},
+		-- Highlight on yank
+		{ "TextYankPost", "*", "silent! lua vim.highlight.on_yank()" },
 
-        -- Change tabs to be 4 spaces for specific langs
-        {'FileType', 'python', 'setl shiftwidth=4 sts=4 ts=2 etc'},
-        {'FileType', 'php', 'setl shiftwidth=4 sts=4 ts=2 etc'},
+		-- Change tabs to be 4 spaces for specific langs
+		{ "FileType", "python", "setl shiftwidth=4 sts=4 ts=2 etc" },
+		{ "FileType", "php", "setl shiftwidth=4 sts=4 ts=2 etc" },
 
-        -- Auto-resize splits when Vim gets resized
-        {'VimResized', '*', 'wincmd ='},
+		-- Auto-resize splits when Vim gets resized
+		{ "VimResized", "*", "wincmd =" },
 
-        -- Update a buffer's contents on focus if it changed outside of Vim
-        {'FocusGained', '*', ':checktime'},
-        {'BufEnter', '*', ':checktime'},
-    }
+		-- Update a buffer's contents on focus if it changed outside of Vim
+		{ "FocusGained", "*", ":checktime" },
+		{ "BufEnter", "*", ":checktime" },
+
+		-- Keep cursorline centered on move
+		{ "CursorMoved", "*", ':exec "norm zz"' },
+	},
 })
-
