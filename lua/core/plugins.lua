6f5ec72a56b727ada26449b9f4e52cf4816dc080
Fred White <fred-white@hotmail.co.uk>
2022-10-15 16:16:38 +0100

    feat(delaytrain): train to stop using hjkl for navigation
    this'll help me start to be better with navigation, will slowly tweak the settings as i see fit
    
    fix #54

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -119,6 +119,7 @@ return packer.startup(function(use)
 	use("segeljakt/vim-silicon") -- https://github.com/segeljakt/vim-silicon
 	use("gaoDean/autolist.nvim") -- https://github.com/gaoDean/autolist.nvim
 	use("smjonas/inc-rename.nvim") -- https://github.com/smjonas/inc-rename.nvim
+	use("ja-ford/delaytrain.nvim") -- https://github.com/ja-ford/delaytrain.nvim
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
