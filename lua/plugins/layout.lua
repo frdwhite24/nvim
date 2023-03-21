bec8f529002085acb97452bdb9c7c1d312495b77
Fred White <fred-white@hotmail.co.uk>
2023-03-21 19:28:36 +0100

    feat(notify): add notify plugin back in

--- lua/plugins/layout.lua
+++ lua/plugins/layout.lua
@@ -23,41 +23,48 @@ return {
             }
         }
     }, {
-        "hoob3rt/lualine.nvim", -- https://github.com/nvim-lualine/lualine.nvim
-        dependencies = "kyazdani42/nvim-web-devicons"
-    }, {
-        "kyazdani42/nvim-web-devicons", -- https://github.com/kyazdani42/nvim-web-devicons
-        lazy = true
-    }, {
-        "kyazdani42/nvim-tree.lua", -- https://github.com/kyazdani42/nvim-tree.lua
-        lazy = true,
-        cmd = "NvimTreeToggle",
-        keys = {
-            {"<leader>e", "<CMD>NvimTreeToggle<CR>", desc = "Toggle file tree"}
+    "hoob3rt/lualine.nvim", -- https://github.com/nvim-lualine/lualine.nvim
+    dependencies = "kyazdani42/nvim-web-devicons"
+}, {
+    "kyazdani42/nvim-web-devicons", -- https://github.com/kyazdani42/nvim-web-devicons
+    lazy = true
+}, {
+    "kyazdani42/nvim-tree.lua", -- https://github.com/kyazdani42/nvim-tree.lua
+    lazy = true,
+    cmd = "NvimTreeToggle",
+    keys = {
+        { "<leader>e", "<CMD>NvimTreeToggle<CR>", desc = "Toggle file tree" }
+    },
+    opts = {
+        open_on_setup = false,
+        auto_reload_on_write = true,
+        update_focused_file = { enable = true, update_cwd = false },
+        git = { enable = true, ignore = false, timeout = 500 },
+        view = {
+            width = 60,
+            hide_root_folder = true,
+            side = 'right',
+            number = false,
+            relativenumber = true,
+            signcolumn = "no"
         },
-        opts = {
-            open_on_setup = false,
-            auto_reload_on_write = true,
-            update_focused_file = {enable = true, update_cwd = false},
-            git = {enable = true, ignore = false, timeout = 500},
-            view = {
-                width = 60,
-                hide_root_folder = true,
-                side = 'right',
-                number = false,
-                relativenumber = true,
-                signcolumn = "no"
-            },
-            actions = {open_file = {quit_on_open = true}}
-        }
-    }, {
-        "petertriho/nvim-scrollbar", -- https://github.com/petertriho/nvim-scrollbar
-        dependencies = {"kevinhwang91/nvim-hlslens"},
+        actions = { open_file = { quit_on_open = true } }
+    }
+}, {
+    "petertriho/nvim-scrollbar", -- https://github.com/petertriho/nvim-scrollbar
+    dependencies = { "kevinhwang91/nvim-hlslens" },
+    config = function()
+        require('scrollbar').setup()
+        require("scrollbar.handlers.search").setup({
+            override_lens = function()
+            end
+        })
+    end
+},
+    {
+        "rcarriga/nvim-notify", -- https://github.com/rcarriga/nvim-notify
         config = function()
-            require('scrollbar').setup()
-            require("scrollbar.handlers.search").setup({
-                override_lens = function() end
-            })
+            vim.notify = require('notify')
         end
     }
 }
