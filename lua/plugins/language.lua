96abb8fa2f3591e0074cf7222d6ec96150a049ce
Fred White <fred-white@hotmail.co.uk>
2023-03-22 10:23:51 +0100

    feat(type inspector): trying out new inspector for TS types

--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -9,7 +9,7 @@ return {
             "saadparwaiz1/cmp_luasnip", "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-nvim-lua", "L3MON4D3/LuaSnip",
             "rafamadriz/friendly-snippets",
-            {"lukas-reineke/lsp-format.nvim", config = true}
+            { "lukas-reineke/lsp-format.nvim", config = true }
         },
         config = function()
             local lsp = require("lsp-zero")
@@ -25,7 +25,7 @@ return {
 
             lsp.setup_nvim_cmp({
                 mapping = cmp.mapping.preset.insert({
-                    ["<Tab>"] = cmp.mapping.confirm({select = true}),
+                    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
                     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(),
@@ -35,14 +35,14 @@ return {
                         else
                             fallback()
                         end
-                    end, {"i", "s"}),
+                    end, { "i", "s" }),
                     ["<C-k>"] = cmp.mapping(function(fallback)
                         if luasnip.jumpable(-1) then
                             luasnip.jump(-1)
                         else
                             fallback()
                         end
-                    end, {"i", "s"})
+                    end, { "i", "s" })
                 })
             })
 
@@ -55,81 +55,99 @@ return {
                 }
             })
 
+            require("lspconfig")["tsserver"].setup({
+                on_attach = function(client, bufnr)
+                    require("twoslash-queries").attach(client, bufnr)
+                end,
+            })
+
             lsp.on_attach(function(client, bufnr)
-                lsp.default_keymaps({buffer = bufnr})
+                lsp.default_keymaps({ buffer = bufnr })
                 require("lsp-format").on_attach(client, bufnr)
 
                 local nmap = function(keys, func, desc)
                     if desc then desc = "LSP: " .. desc end
 
                     vim.keymap.set("n", keys, func,
-                                   {buffer = bufnr, desc = desc})
+                        { buffer = bufnr, desc = desc })
                 end
 
                 local builtin = require("telescope.builtin")
                 nmap("gd", function()
-                    builtin.lsp_definitions({show_line = false})
+                    builtin.lsp_definitions({ show_line = false })
                 end, "[G]o to [D]efinition")
                 nmap("gD", function()
-                    builtin.lsp_type_definitions({show_line = false})
+                    builtin.lsp_type_definitions({ show_line = false })
                 end, "[G]o to type [D]efinition")
                 nmap("gr", function()
-                    builtin.lsp_references({show_line = false})
+                    builtin.lsp_references({ show_line = false })
                 end, "[G]o [R]eferences")
                 nmap("K", vim.lsp.buf.hover, "Hover documentation")
                 nmap("<C-k>", vim.lsp.buf.signature_help,
-                     "Signature documentation")
+                    "Signature documentation")
                 nmap("gi", vim.lsp.buf.implementation,
-                     "[G]o to [I]mplementation")
+                    "[G]o to [I]mplementation")
                 nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
                 nmap("[d", vim.diagnostic.goto_prev,
-                     "Go to previous [D]iagnostic message")
+                    "Go to previous [D]iagnostic message")
                 nmap("]d", vim.diagnostic.goto_next,
-                     "Go to next [D]iagnostic message")
+                    "Go to next [D]iagnostic message")
                 nmap("gl", vim.diagnostic.open_float,
-                     "[G]o to [L]ine diagnostic float")
+                    "[G]o to [L]ine diagnostic float")
             end)
 
-            require("mason.settings").set({ui = {border = "rounded"}})
+            require("mason.settings").set({ ui = { border = "rounded" } })
             lsp.setup()
 
-            vim.diagnostic.config({virtual_text = true})
+            vim.diagnostic.config({ virtual_text = true })
 
             -- this inserts () after selecting a function or method item
             local cmp_autopairs = require("nvim-autopairs.completion.cmp")
             cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
         end
     }, {
-        "j-hui/fidget.nvim", -- https://github.com/j-hui/fidget.nvim
-        config = true
-    }, {
-        "jose-elias-alvarez/null-ls.nvim", -- https://github.com/jose-elias-alvarez/null-ls.nvim
-        config = function()
-            local null_ls = require("null-ls")
-            local sources = {null_ls.builtins.formatting.prettier}
+    "j-hui/fidget.nvim", -- https://github.com/j-hui/fidget.nvim
+    config = true
+}, {
+    "jose-elias-alvarez/null-ls.nvim", -- https://github.com/jose-elias-alvarez/null-ls.nvim
+    config = function()
+        local null_ls = require("null-ls")
+        local sources = { null_ls.builtins.formatting.prettier }
 
-            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-            local on_attach = function(client, bufnr)
-                if client.supports_method("textDocument/formatting") then
-                    vim.api.nvim_clear_autocmds({
-                        group = augroup,
-                        buffer = bufnr
-                    })
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = augroup,
-                        buffer = bufnr,
-                        callback = function()
-                            vim.lsp.buf.format({
-                                bufnr = bufnr,
-                                timeout_ms = 1000
-                            })
-                        end
-                    })
-                end
+        local on_attach = function(client, bufnr)
+            if client.supports_method("textDocument/formatting") then
+                vim.api.nvim_clear_autocmds({
+                    group = augroup,
+                    buffer = bufnr
+                })
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = augroup,
+                    buffer = bufnr,
+                    callback = function()
+                        vim.lsp.buf.format({
+                            bufnr = bufnr,
+                            timeout_ms = 1000
+                        })
+                    end
+                })
             end
-
-            null_ls.setup({sources = sources, on_attach = on_attach})
         end
-    }
+
+        null_ls.setup({ sources = sources, on_attach = on_attach })
+    end
+}, {
+    "marilari88/twoslash-queries.nvim", -- https://github.com/marilari88/twoslash-queries.nvim
+    config = true,
+    lazy = true,
+    cmd = "InspectTwoslashQueries",
+    keys = {
+        {
+            "<leader>it",
+            "<CMD>InspectTwoslashQueries<CR>",
+            desc = "[I]nspect [T]ype"
+        }
+    },
+}
 }
