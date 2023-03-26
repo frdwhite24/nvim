be19593b914df91f3420f09af380e4fdc6a9288c
Fred White <fred-white@hotmail.co.uk>
2023-03-26 20:28:13 +0200

    feat(theme): try out carbonfox in the end, the others did not speak to              me as much
    related #55

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -1,35 +1,35 @@
 return {
     {
         "EdenEast/nightfox.nvim", -- https://github.com/EdenEast/nightfox.nvim
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+        lazy = false,             -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000,          -- make sure to load this before all the other start plugins
         config = function()
             vim.opt.termguicolors = true
-            vim.cmd.colorscheme("nightfox")
+            vim.cmd.colorscheme("carbonfox")
         end
     }, {
-        "nvim-treesitter/nvim-treesitter", -- https://github.com/nvim-treesitter/nvim-treesitter
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup {
-                ensure_installed = {
-                    "bash", "dockerfile", "graphql", "html", "help",
-                    "javascript", "json", "lua", "php", "python", "markdown",
-                    "rust", "css", "scss", "tsx", "typescript", "yaml"
-                },
-                sync_install = false,
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false
-                }
+    "nvim-treesitter/nvim-treesitter", -- https://github.com/nvim-treesitter/nvim-treesitter
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup {
+            ensure_installed = {
+                "bash", "dockerfile", "graphql", "html", "help",
+                "javascript", "json", "lua", "php", "python", "markdown",
+                "rust", "css", "scss", "tsx", "typescript", "yaml"
+            },
+            sync_install = false,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false
             }
-        end
-    }, {
-        "folke/twilight.nvim", -- https://github.com/folke/twilight.nvim
-        lazy = true,
-        cmd = 'Twilight',
-        config = true,
-        keys = {{"<leader>z", "<CMD>Twilight<CR>", desc = "Toggle zen mode"}}
-    }
+        }
+    end
+}, {
+    "folke/twilight.nvim", -- https://github.com/folke/twilight.nvim
+    lazy = true,
+    cmd = 'Twilight',
+    config = true,
+    keys = { { "<leader>z", "<CMD>Twilight<CR>", desc = "Toggle zen mode" } }
+}
 }
