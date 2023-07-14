ad521b070e05898f89237949e99b526679c2e8cb
Fred White <fred-white@hotmail.co.uk>
2023-07-14 09:54:43 +0200

    build(deps): bump versons

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -7,25 +7,25 @@ local yank_group = augroup('HighlightYank', {})
 autocmd("BufWinEnter", {
     group = MyGroup,
     pattern = "*",
-    callback = function() vim.opt.formatoptions:remove({"c", "o"}) end
+    callback = function() vim.opt.formatoptions:remove({ "c", "o" }) end
 })
-autocmd({"BufRead", "BufNewFile"}, {
+autocmd({ "BufRead", "BufNewFile" }, {
     group = MyGroup,
     pattern = "*",
-    callback = function() vim.opt.formatoptions:remove({"c", "o", "r"}) end
+    callback = function() vim.opt.formatoptions:remove({ "c", "o", "r" }) end
 })
 
 -- Auto-resize splits when Vim gets resized
-autocmd("VimResized", {group = MyGroup, pattern = "*", command = "wincmd ="})
+autocmd("VimResized", { group = MyGroup, pattern = "*", command = "wincmd =" })
 
 -- Update a buffer's contents on focus if it changed outside of Vim
-autocmd({"FocusGained", "BufEnter"},
-        {group = MyGroup, pattern = "*", command = "checktime"})
+autocmd({ "FocusGained", "BufEnter" },
+    { group = MyGroup, pattern = "*", command = "checktime" })
 
 autocmd('TextYankPost', {
     group = yank_group,
     pattern = '*',
     callback = function()
-        vim.highlight.on_yank({higroup = 'IncSearch', timeout = 150})
+        vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 150 })
     end
 })
