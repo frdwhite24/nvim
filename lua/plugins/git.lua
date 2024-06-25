b58859a89489651343d86fa0920a584d1426f645
Fred White <fredwhite.dev@gmail.com>
2024-06-25 10:27:23 +0200

    fix(gitsigns): wrong brackets

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -59,56 +59,54 @@ return {
             row = 0,
             col = 1
         },
-    },
-    on_attach = function(bufnr)
-        local gitsigns = require('gitsigns')
+        on_attach                    = function(bufnr)
+            local gitsigns = require('gitsigns')
 
-        local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
+            local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+            end
+
+            -- Navigation
+            map('n', ']c', function()
+                if vim.wo.diff then
+                    vim.cmd.normal({ ']c', bang = true })
+                else
+                    gitsigns.nav_hunk('next')
+                end
+            end)
+
+            map('n', '[c', function()
+                if vim.wo.diff then
+                    vim.cmd.normal({ '[c', bang = true })
+                else
+                    gitsigns.nav_hunk('prev')
+                end
+            end)
+
+            -- Actions
+            map('n', '<leader>hs', gitsigns.stage_hunk)
+            map('n', '<leader>hr', gitsigns.reset_hunk)
+            map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+            map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+            map('n', '<leader>hS', gitsigns.stage_buffer)
+            map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+            map('n', '<leader>hR', gitsigns.reset_buffer)
+            map('n', '<leader>hp', gitsigns.preview_hunk)
+            map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end)
+            map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+            map('n', '<leader>hd', gitsigns.diffthis)
+            map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+            map('n', '<leader>td', gitsigns.toggle_deleted)
+
+            -- Text object
+            map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+
+            require('gitsigns').setup()
+            require("scrollbar.handlers.gitsigns").setup()
         end
-
-        -- Navigation
-        map('n', ']c', function()
-            if vim.wo.diff then
-                vim.cmd.normal({ ']c', bang = true })
-            else
-                gitsigns.nav_hunk('next')
-            end
-        end)
-
-        map('n', '[c', function()
-            if vim.wo.diff then
-                vim.cmd.normal({ '[c', bang = true })
-            else
-                gitsigns.nav_hunk('prev')
-            end
-        end)
-
-        -- Actions
-        map('n', '<leader>hs', gitsigns.stage_hunk)
-        map('n', '<leader>hr', gitsigns.reset_hunk)
-        map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-        map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-        map('n', '<leader>hS', gitsigns.stage_buffer)
-        map('n', '<leader>hu', gitsigns.undo_stage_hunk)
-        map('n', '<leader>hR', gitsigns.reset_buffer)
-        map('n', '<leader>hp', gitsigns.preview_hunk)
-        map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end)
-        map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-        map('n', '<leader>hd', gitsigns.diffthis)
-        map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
-        map('n', '<leader>td', gitsigns.toggle_deleted)
-
-        -- Text object
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-
-        local scrollbar_status_ok, _ = pcall(require, "scrollbar")
-        if not scrollbar_status_ok then return end
-
-        require("scrollbar.handlers.gitsigns").setup()
-    end
+    },
 
 }, {
     "f-person/git-blame.nvim", -- https://github.com/f-person/git-blame.nvim#configuration
