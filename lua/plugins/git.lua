00a541e658dfe13c04f2a10aaacf0bbe286186ff
Fred White <fredwhite.dev@gmail.com>
2024-07-08 11:12:32 +0200

    feat(deps): upgrade

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -20,7 +20,7 @@ return {
     "lewis6991/gitsigns.nvim", -- https://github.com/lewis6991/gitsigns.nvim
     dependencies = { "petertriho/nvim-scrollbar" },
     opts = {
-        signs                        = {
+        signs               = {
             add          = { text = '┃' },
             change       = { text = '┃' },
             delete       = { text = '_' },
@@ -28,30 +28,22 @@ return {
             changedelete = { text = '~' },
             untracked    = { text = '┆' },
         },
-        signcolumn                   = true,
-        numhl                        = true,
-        linehl                       = false,
-        word_diff                    = false,
-        watch_gitdir                 = {
+        signcolumn          = true,
+        numhl               = true,
+        linehl              = false,
+        word_diff           = false,
+        watch_gitdir        = {
             follow_files = true,
             interval = 1000
         },
-        auto_attach                  = true,
-        attach_to_untracked          = true,
-        current_line_blame           = false,
-        current_line_blame_opts      = {
-            virt_text = true,
-            virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-            delay = 1000,
-            ignore_whitespace = false,
-            virt_text_priority = 100,
-        },
-        current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
-        sign_priority                = 6,
-        update_debounce              = 100,
-        status_formatter             = nil,   -- Use default
-        max_file_length              = 40000, -- Disable if file is longer than this (in lines)
-        preview_config               = {
+        auto_attach         = true,
+        attach_to_untracked = true,
+        current_line_blame  = false,
+        sign_priority       = 6,
+        update_debounce     = 100,
+        status_formatter    = nil,       -- Use default
+        max_file_length     = 40000,     -- Disable if file is longer than this (in lines)
+        preview_config      = {
             -- Options passed to nvim_open_win
             border = 'single',
             style = 'minimal',
@@ -59,7 +51,7 @@ return {
             row = 0,
             col = 1
         },
-        on_attach                    = function(bufnr)
+        on_attach           = function(bufnr)
             local gitsigns = require('gitsigns')
 
             local function map(mode, l, r, opts)
