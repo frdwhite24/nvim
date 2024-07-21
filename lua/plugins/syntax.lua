1bb2542bc2a115d6231acdfe2a939bdbca605771
Fred White <fredwhite.dev@gmail.com>
2024-07-21 16:50:27 +0200

    feat(deps): bump

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -1,13 +1,22 @@
 return {
     {
-        "EdenEast/nightfox.nvim", -- https://github.com/EdenEast/nightfox.nvim
-        lazy = false,             -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000,          -- make sure to load this before all the other start plugins
+        "folke/tokyonight.nvim", -- https://github.com/folke/tokyonight.nvim
+        lazy = false,
+        priority = 1000,
+        opts = {},
         config = function()
-            vim.opt.termguicolors = true
-            vim.cmd.colorscheme("carbonfox")
+            vim.cmd.colorscheme('tokyonight')
         end
     }, {
+
+    --     "EdenEast/nightfox.nvim", -- https://github.com/EdenEast/nightfox.nvim
+    --     lazy = false,             -- make sure we load this during startup if it is your main colorscheme
+    --     priority = 1000,          -- make sure to load this before all the other start plugins
+    --     config = function()
+    --         vim.opt.termguicolors = true
+    --         vim.cmd.colorscheme("carbonfox")
+    --     end
+    -- }, {
     "nvim-treesitter/nvim-treesitter", -- https://github.com/nvim-treesitter/nvim-treesitter
     build = ":TSUpdate",
     dependencies = {
