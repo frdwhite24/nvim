7d27a113b454c5a91ca51f3f929445cc57350adb
Fred White <fredwhite.dev@gmail.com>
2024-09-16 11:19:36 +0200

    fix(flash): disable multiline ftFT behaviour

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -67,7 +67,8 @@ return {
                 enabled = true
             },
             char = {
-                jump_labels = true
+                jump_labels = true,
+                multi_line = false,
             }
         },
     },
