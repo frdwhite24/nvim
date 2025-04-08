07d68aca45bf39960ec870ddbcae027001109050
Fred White <fredwhite.dev@gmail.com>
2025-04-08 12:32:10 +0200

    feat(gx enhanced): fantastic new plugin to enhance the gx behaviour,opening deps, browsing words etc

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -3,5 +3,13 @@ return {
         "dstein64/vim-startuptime", -- https://github.com/dstein64/vim-startuptime
         cmd = "StartupTime",
         init = function() vim.g.startuptime_tries = 10 end
-    }
-}
+    }, {
+    "chrishrb/gx.nvim", -- https://github.com/chrishrb/gx.nvim
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    cmd = { "Browse" },
+    init = function()
+        vim.g.netrw_nogx = 1                    -- disable netrw gx
+    end,
+    dependencies = { "nvim-lua/plenary.nvim" }, -- Required for Neovim < 0.10.0
+    config = true,
+} }
